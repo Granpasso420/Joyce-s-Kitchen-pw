0a45838f8881d05429f92b1889aaf2f2de589541
EleonoraS97 <eleonora297@hotmail.it>
2022-12-22 17:36:07 +0100

    foto..

--- joyce/src/main/java/it/corso/dao/RecipeDao.java
+++ joyce/src/main/java/it/corso/dao/RecipeDao.java
@@ -1,9 +1,13 @@
 package it.corso.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import it.corso.model.Recipe;
 
 public interface RecipeDao extends CrudRepository<Recipe, Integer>{
 
+	@Query(value = "select max(id_recipe) from recipes", nativeQuery = true)
+	
+	int getLastId();
 }
