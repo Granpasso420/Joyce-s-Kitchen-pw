60ccfe89094d067d3a0fdc05c6c8f9b1468ea268
rosebud4567 <miccolimichela2001@gmail.com>
2022-12-27 21:46:06 +0100

    responsive

--- ProjectWork_20Dic/Javascript/HomePage2.js
+++ ProjectWork_20Dic/Javascript/HomePage2.js
@@ -2,3 +2,22 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+function menu(){
+var checkbox= document.getElementById("checkbox");
+var navBar = document.getElementById('navBar');
+var X=document.getElementById("X");
+var panino= document.getElementById("panino");
+
+if (checkbox.checked == true){
+    navBar.classList.add("navbar3");
+    X.classList.add("Xvisible");
+    panino.classList.remove("paninoDopo");
+    
+
+} else{
+    navBar.classList.remove("navbar3");
+    X.classList.remove("Xvisible");
+    panino.classList.add("paninoDopo");
+}
+}
