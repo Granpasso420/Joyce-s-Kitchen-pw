8383424f9777efd91ada7bff4f1e983342f32122
rosebud4567 <miccolimichela2001@gmail.com>
2023-01-02 15:45:03 +0100

    aggiornamento

--- joyce/src/main/webapp/static/javascript/HomePage2.js
+++ joyce/src/main/webapp/static/javascript/HomePage2.js
@@ -2,3 +2,21 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+function menu(){
+var navBar = document.getElementById('navBar');
+
+var panino= document.getElementById("panino");
+
+    navBar.classList.add("navbar3");
+    X.classList.add("Xvisible");
+    panino.classList.remove("paninoDopo");
+
+}
+
+function chiudere(){
+    var X=document.getElementById("X");
+    navBar.classList.remove("navbar3");
+    X.classList.remove("Xvisible");
+    panino.classList.add("paninoDopo");
+}
