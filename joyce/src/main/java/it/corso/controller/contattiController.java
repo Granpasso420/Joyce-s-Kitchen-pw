b0a222399680d3a1dff42ff3622e3623ed1380d5
EleonoraS97 <eleonora297@hotmail.it>
2023-01-02 10:49:51 +0100

    contatti e controller

---
+++ joyce/src/main/java/it/corso/controller/contattiController.java
@@ -0,0 +1,17 @@
+package it.corso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contatti")
+public class contattiController {
+	
+	@GetMapping
+	public String getPage(Model model) {
+		return "contatti";
+	}
+
+}
